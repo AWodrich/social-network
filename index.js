c85db36c38478ea77198bc126cc3d0af056f31b5
AWodrich <anjawodrich@gmail.com>
2017-11-28 15:00:01 +0100

    beginning notes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+// compression is a core epress middleware, to compress text, that is being sent back to the browser.
+// the browser knows automatically how to decompress the text.
 const compression = require('compression');
+
+// every time we need to regenerate the entire applicaton.
 
 app.use(compression());
 
+// test if we are in production, which would be heroku.
+// when code is bundle.js, then go to a different server, here it would be localhost:8081;
 if (process.env.NODE_ENV != 'production') {
     app.use('/bundle.js', require('http-proxy-middleware')({
         target: 'http://localhost:8081/'
--- src/start.js
+++ src/start.js
@@ -1,13 +1,50 @@
 import React from 'react';
+// React library ReactDom for interacting with the DOM
 import ReactDOM from 'react-dom';
 
+
+class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('this.props is', this.props);
+
+        this.state = {
+            name:'Kitty'
+        };
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState({
+            name:'World'
+        })
+    }
+
+    render() {
+        return(
+        <div>
+            <div onClick={this.handleClick}>Hello, <Receiver name={this.state.name} />!</div>
+            <PrettyInput changeText={name => this.setState({ name }) }/>
+        </div>
+        );
+    }
+}
+
+
 ReactDOM.render(
-    <HelloWorld />,
+    <Hello name='Kitty' />,
     document.querySelector('main')
 );
 
-function HelloWorld() {
+
+function PrettyInput() {
+    console.log('in here');
+    var style={border: '1px dotted red', fontSize: '20px', color: 'green'}
     return (
-        <div>Hello, World!</div>
-    );
+        <input style={style} type="text">
+    )
+}
+
+function Receiver(props) {
+    return <strong> {props.name}</strong>
 }
