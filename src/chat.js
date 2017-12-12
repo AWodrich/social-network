25f657a7285f96b66a6d184f122b77077e206f89
AWodrich <anjawodrich@gmail.com>
2017-12-12 14:01:32 +0100

    part9 done

--- src/chat.js
+++ src/chat.js
@@ -20,51 +20,42 @@ class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidUpdate() {
     this.elem.scrollTop = this.elem.scrollHeight - this.elem.clientHeight;
+    this.textArea.value =''
   }
 
   componentDidMount() {
     this.props.getMessages();
   }
 
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-
-    }, () => {
-    //   console.log('new state', this.state);
-    })
-  }
 
   render() {
       console.log('props?', this.props);
-      let messageList =[];
+      var messageList =[];
       if (this.props.messages) {
           console.log('////', this.props.messages);
-          messageList = this.props.messages.map(message => {
+          this.props.messages.map(message => {
               console.log('message', message);
               messageList.push(
                   <div>
-                  <p>{message.first} says: {message.message}</p>
+                    <p>{message.first} says: {message.message}</p>
                   </div>
               )
           })
+          console.log('messageList',messageList,'????????');
       }
 
-    //   console.log('messageList',messageList );
 
     // index of array
 
         return (
             <div id='chat'>
-                {this.props.messages &&<div id="chatBox" ref={elem => this.elem = elem}>{messageList}</div>}
-                {!this.props.messages &&<div id="chatBox" ref={elem => this.elem = elem}></div> }
-                <textarea onChange={this.handleChange} name="composedMessage" id="" cols="30" rows="10"></textarea>
-                <button onClick={() => this.props.postMessage(this.state.composedMessage)}>Post</button>
+            <div id="chatBox" ref={elem => this.elem = elem}>{this.props.messages && messageList}</div>
+            <textarea ref={textArea => this.textArea = textArea} name="composedMessage"></textarea>
+            <button onClick={() => this.props.postMessage(this.textArea.value)}>Post</button>
             </div>
         )
     }
